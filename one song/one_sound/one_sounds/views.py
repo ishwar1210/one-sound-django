d2758c801db4b5908eed08f3ff0d41685f93ba9d
ishwar khairnar <ishwarkhairnar389@gmail.com>
2025-07-26 21:03:12 +0530

    Update views.py

--- one song/one_sound/one_sounds/views.py
+++ one song/one_sound/one_sounds/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from albums.models import Album
 # Create your views here.
 
+
 def index(request):
     context = {"albums": Album.objects.all()}
     return render(request, 'index.html', context)
